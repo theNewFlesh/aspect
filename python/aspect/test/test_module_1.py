3be07c0eee2a611cd46ffeb89f5b9b507c40c0f9
Alex Braun <alexander.g.braun@gmail.com>
2016-03-06 19:14:15 -0800

    added unit testing

--- python/aspect/test/test_module_1.py
+++ python/aspect/test/test_module_1.py
@@ -1,83 +1,78 @@
-from aspect.test.utils import aspect
-import re
-from itertools import *
+from __future__ import print_function
+from aspect.test.tests import aspect
 # ------------------------------------------------------------------------------
 
-some_var = re.compile('ksgfhdj')
-
 class Test1(object):
-    '''Test class docstring'''
-    class_attr = 99
-    _class_attr = 100
-    __class_attr = 101
+	'''Test class docstring'''
+	class_attr = 99
+	_class_attr = 100
+	__class_attr = 101
 
-    def __init__(self):
-        self.instance_attr = 'a'
-        self._instance_attr = 'b'
-        self.__instance_attr = 'c'
-    
-    @property
-    def property_(self):
-        return self.instance_attr
-    
-    @property
-    def _property(self):
-        return self._instance_attr
-    
-    @property
-    def __property(self):
-        return self.__instance_attr
-    
-    @aspect.deregister
-    def deregistered_method(self, arg):
-        return 'deregistered method fired'
-    
-    def method1(self, arg):
-        return 'method1 fired'
+	def __init__(self):
+		self.instance_attr = 'a'
+		self._instance_attr = 'b'
+		self.__instance_attr = 'c'
+	
+	@property
+	def property_(self):
+		return 'the_value_of_property_'
+	
+	@property
+	def _property(self):
+		return self._instance_attr
+	
+	@property
+	def __property(self):
+		return self.__instance_attr
+	
+	@aspect.deregister
+	def deregistered_method(self, arg):
+		return 'deregistered method fired'
+	
+	def method1(self, arg):
+		return 'method1 fired'
 
-    def method2(self, arg1of2, arg2of2):
-        return 'method2 fired'
+	def method2(self, arg1of2, arg2of2):
+		return 'method2 fired'
 
-    def method3(self, arg1of1, kwarg1of2=1, kwarg2of2=2):
-        return 'method3 fired'
+	def method3(self, arg1of1, kwarg1of2=1, kwarg2of2=2):
+		return 'method3 fired'
 
-    def method4(self, arg1of2, arg2of2, kwarg1of2=1, kwarg2of2=2):
-        return 'method4 fired'
+	def method4(self, arg1of2, arg2of2, kwarg1of2=1, kwarg2of2=2):
+		return 'method4 fired'
 
-    def method5(self, arg1of2, arg2of2, kwarg1of2=1, kwarg2of2=2, **kwargs):
-        return 'method5 fired'
-    
-    def _method(self, kwarg1of1=1):
-        return kwarg
-    
-    def __method(self, *args, **kwargs):
-        return args, kwargs
+	def method5(self, arg1of2, arg2of2, kwarg1of2=1, kwarg2of2=2, **kwargs):
+		return 'method5 fired'
+	
+	def _method(self, kwarg1of1=1):
+		return kwarg
+	
+	def __method(self, *args, **kwargs):
+		return args, kwargs
 # ------------------------------------------------------------------------------    
 
-def module_func1(apple, banana, kiwi=3):
-    '''test docstring'''
-    print(apple, banana, kiwi)
-    print('it works')
+def module1_func1(arg1of2, arg2of2, **kwargs):
+	'''module1_func1 docstring'''
+	return arg1of2, arg2of2, kwargs
 
 @aspect.deregister
-def deregistered_module_func1(apple, banana, kiwi=3):
-    '''test docstring'''
-    print(apple, banana, kiwi)
-    print('it works')
+def module1_deregistered_func2(arg1of2, arg2of2, kwarg1of1=3):
+	'''module1_deregistered_func2 docstring'''
+	return arg1of2, arg2of2, kwarg1of1
 
 aspect.register(__name__)
 # ------------------------------------------------------------------------------
 
 def main():
-    '''
-    Run help if called directly
-    '''
-    
-    import __main__
-    help(__main__)
+	'''
+	Run help if called directly
+	'''
+	
+	import __main__
+	help(__main__)
 # ------------------------------------------------------------------------------
 
-__all__ = ['Test1', 'module_func1', 'deregistered_module_func1']
+__all__ = ['Test1', 'module1_func1', 'module1_deregistered_func2']
 
 if __name__ == '__main__':
-    main()
+	main()
---
+++ python/aspect/test/tests.py
@@ -0,0 +1,117 @@
+from __future__ import with_statement, print_function
+from nose import with_setup
+from nose.tools import assert_raises
+
+from aspect.core.aspect import Aspect
+aspect = Aspect()
+
+from aspect.test.test_module_1 import Test1
+from aspect.test.test_module_2 import Test2
+from schematics.exceptions import ValidationError
+# ------------------------------------------------------------------------------
+
+def id_setup():
+	id_ = 1111111111
+	spec = dict(
+		module='aspect.test.test_module_1',
+		class_='Test1',
+		instance=Test1()
+	)
+	aspect._library[id_] = spec
+
+def id_teardown():
+	id_ = 1111111111
+	del aspect._library[id_]
+
+def create_001_test():
+	spec = dict(
+		action='create',
+		module='aspect.test.test_module_1',
+		class_='Test1',
+		args=['name'],
+		kwargs=dict(kwarg1=10, kwarg2=11)
+	)
+	response = aspect.request(spec)
+	assert(isinstance(response, int) and len(str(response)) == 10)
+
+@with_setup(id_setup, id_teardown)
+def read_001_test():
+	spec = dict(
+		action='read',
+		id_=1111111111,
+		attribute='property_'
+	)
+	response = aspect.request(spec)
+	assert(response == 'the_value_of_property_')
+
+@with_setup(id_setup, id_teardown)
+def update_001_test():
+	spec = dict(
+		action='update',
+		id_=1111111111,
+		attribute='class_attr',
+		value='new_value'
+	)
+	response = aspect.request(spec)
+	assert(response == True)
+
+@with_setup(id_setup)
+def delete_001_test():
+	spec = dict(
+		action='delete',
+		id_=1111111111
+	)
+	response = aspect.request(spec)
+	assert(response == True)
+
+@with_setup(id_setup, id_teardown)
+def execute_method_001_test():
+	spec = dict(
+		action='execute',
+		id_=1111111111,
+		method='method1',
+		args=[1],
+		kwargs={}
+	)
+	response = aspect.request(spec)
+	assert(response == 'method1 fired')
+
+def execute_function_001_test():
+	spec = dict(
+		action='execute',
+		module='aspect.test.test_module_1',
+		function='module1_func1',
+		args=[1,2],
+		kwargs={}
+	)
+	response = aspect.request(spec)
+	print(response)
+	assert( response == (1,2,{}) )
+
+def execute_deregistered_function_001_test():
+	spec = dict(
+		action='execute',
+		module='aspect.test.test_module_1',
+		function='module1_deregistered_func2',
+		args=[1,2],
+		kwargs={}
+	)
+	with assert_raises(ValidationError):
+		aspect.request(spec)
+# ------------------------------------------------------------------------------
+
+def main():
+	'''
+	Run help if called directly
+	'''
+	
+	import __main__
+	help(__main__)
+# ------------------------------------------------------------------------------
+
+__all__ = [
+	'aspect'
+]
+
+if __name__ == '__main__':
+	main()
