4215123912321c3a0cd505020de461cd59fc50cf
Alex Braun <alexander.g.braun@gmail.com>
2016-03-07 16:23:01 -0800

    added test.utils

--- python/aspect/test/__init__.py
+++ python/aspect/test/__init__.py
@@ -1,3 +1,4 @@
 import tests
 import test_module_1
 import test_module_2
+import utils
--- python/aspect/test/app.py
+++ python/aspect/test/app.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from pprint import pprint
 
 from aspect.test.tests import aspect
+from aspect.test.utils import aspect_to_dataframe
 
 from flask import Flask, request, Response, render_template, jsonify
 app = Flask(__name__)
@@ -15,9 +16,10 @@ def index():
 	if request.method == 'POST':
 		spec = request.get_json(silent=True)
 		response = aspect.request(spec, json_errors=True)
+		print(aspect_to_dataframe(aspect))
 		print()
-		pprint(aspect._library)
-		print()
+		# pprint(aspect._library)
+		# print()
 
 		return jsonify(response=response)
 		# return render_template('index.html', response)
