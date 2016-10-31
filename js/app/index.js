f0def140757403f4a58d97fd8d2a93e3943e06b8
Alex Braun <alexander.g.braun@gmail.com>
2016-10-30 20:02:21 -0700

    replaced web stuff with working dashboard/toolbar html/css

--- js/app/index.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDom = require('react-dom');
-var routes = require('./config/routes');
-// -----------------------------------------------------------------------------
-
-ReactDom.render( routes, document.getElementById('app') );
