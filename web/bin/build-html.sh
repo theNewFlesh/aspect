2af9b76a890a2fe514ec5b5f7aa6c4d7fb89ef75
Alex Braun <alexander.g.braun@gmail.com>
2017-04-30 04:49:22 -0700

    fixed left right scrolling columns

--- web/bin/build-html.sh
+++ web/bin/build-html.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 rm static/css/style.css
-jinja2 templates/style.css.j2 yaml/data.yml --format=yaml >> static/css/style.css
+jinja2 templates/style.css.j2 static/test-data.yml --format=yaml >> static/css/style.css
 rm static/index.html
-jinja2 templates/index.html.j2 yaml/data.yml --format=yaml >> static/index.html
+jinja2 templates/index.html.j2 static/test-data.yml --format=yaml >> static/index.html
 open static/index.html
